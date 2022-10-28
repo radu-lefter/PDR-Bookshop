c1f080006c4aacf14d44526e0dd71282cf674399
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-10-28 10:01:02 +0100

    initial commit

---
+++ resources/books.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Emma',
+    image: '/images/emma.jpg',
+    description:
+      'A rich girl that lives a confortable life tries to play at matchmaking and finds out she is not terribly good at it.',
+    publisher: 'Penguin',
+    genre: 'romance',
+    price: 9.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Pride and Prejudice',
+    image: '/images/pride_and_prejudice.jpg',
+    description:
+      'Sisters Elisabeth and Jane find true love after battling the prejudice of their society and the pride of the rich men they love.',
+    publisher: 'Penguin',
+    genre: 'romance',
+    price: 8.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Lady Susan',
+    image: '/images/lady_susan.jpg',
+    description:
+      'A beautiful epistolary novel about the exploits and loves of the infamous Lady Susan.',
+    publisher: 'Pearson',
+    genre: 'romance',
+    price: 12.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Sense and sensibility',
+    image: '/images/sense_and_sensibility.jpg',
+    description:
+      'Two sisters try to reconcile reason with love in their search for the ideal husband.',
+    publisher: 'Pearson',
+    genre: 'romance',
+    price: 8.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Mansfield Park',
+    image: '/images/mansfield_park.jpg',
+    description:
+      'A shy girl manages to triumph against the cruelty of a society she does not fit in.',
+    publisher: 'Hachette',
+    genre: 'romance',
+    price: 9.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Persuasion',
+    image: '/images/persuasion.jpg',
+    description:
+      'On the verge of becoming a spsinster, a lucky woman rediscovers a long lost love.',
+    publisher: 'Hachette',
+    genre: 'romance',
+    price: 10.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
